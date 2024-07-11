92819890827e030707071ad93a2549af40e608a6
Bassem <BasemOmar2012@gmail.com>
2024-07-11 21:25:38 +0100

    adding description

--- app/src/main/java/com/example/problemsolvingkotlin/easy/breakingRecords.kt
+++ app/src/main/java/com/example/problemsolvingkotlin/easy/breakingRecords.kt
@@ -1,5 +1,10 @@
 package com.example.problemsolvingkotlin.easy
 
+/**
+ * Maria plays college basketball and wants to go pro. Each season she maintains a record of her play.
+ * She tabulates the number of times she breaks her season record for most points and least points in a game.
+ * Points scored in the first game establish her record for the season, and she begins counting from there.
+ */
 fun breakingRecords(scores: Array<Int>): Array<Int> {
     var min = scores[0]
     var max = scores[0]
